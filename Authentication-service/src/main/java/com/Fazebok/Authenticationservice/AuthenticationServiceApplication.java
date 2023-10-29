7648256976e9a3992381515c1a59245c12931278
aziz0008 <keskesazizpaypal@gmail.com>
2023-10-29 16:00:26 +0100

    content added

---
+++ Authentication-service/src/main/java/com/Fazebok/Authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Fazebok.Authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ Interaction-service/src/test/java/com/Fazebok/Interactionservice/InteractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fazebok.Interactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
